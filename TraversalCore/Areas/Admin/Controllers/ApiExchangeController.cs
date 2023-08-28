c927c9b07848b05d89a19c2d754fc10d72ebf59c
Batuhan Ertuğrul <erbatuhan255@gmail.com>
2023-08-28 14:33:48 +0300

    Rapid api eklendi

---
+++ TraversalCore/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TraversalCore.Areas.Admin.Models;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> bookingExchangeViews = new List<BookingExchangeViewModel2>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apidojo-booking-v1.p.rapidapi.com/currency/get-exchange-rates?base_currency=TRY&languagecode=en-us"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "5ed620b542msh779a437d268921dp110c8djsnb56cbb50c79d" },
+        { "X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+            }
+
+        }
+    }
+} 
---
+++ TraversalCore/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using TraversalCore.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "5ed620b542msh779a437d268921dp110c8djsnb56cbb50c79d" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+
+        }
+    }
+}
